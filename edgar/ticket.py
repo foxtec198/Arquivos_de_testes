3470152172191b66367982c1872f8a393c1af3a5
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-05-23 15:09:27 -0300

    .

--- edgar/ticket.py
+++ edgar/ticket.py
@@ -8,12 +8,12 @@ class App():
         self.nome_arquivo = 'temp_code'
         self.w32 = win32print
         while True:
-            try:
-                self.codigo = input("Bipe o código de barras do produto (ou 'sair' para encerrar): ")
-                self.gerar_cod()
-                break
-            except:
-                print('Valor incorreto, Digite novamente!')   
+            # try:
+            self.codigo = input("Bipe o código de barras do produto (ou 'sair' para encerrar): ")
+            self.gerar_cod()
+                # break
+            # except:
+                # print('Valor incorreto, Digite novamente!')   
     
     def gerar_cod(self):
         self.codigo_de_barras = EAN13(str(self.codigo), writer=ImageWriter())
@@ -21,29 +21,14 @@ class App():
         self.imprimir()
         
     def imprimir(self):
-        lista_impressoras = self.w32.EnumPrinters(2)
-        #print(lista_impressoras)
-        impressora = lista_impressoras[5]
-        impressora = impressora[2]
-        self.w32.SetDefaultPrinter(impressora)
+        defp = win32print.GetDefaultPrinter()
+        prt = win32print.OpenPrinter(defp)
         
-        # Imprimir Titulo
-        nome_empresa = str("RCHLO")
-        self.w32.WritePrinter(impressora, nome_empresa)
-        self.w32.WritePrinter(self.w32.GetDefaultPrinter(), b'\n')
-
-        # Imprimir mensagem
-        mensagem = "Cumpom não fiscal!"
-        self.w32.WritePrinter(self.w32.GetDefaultPrinter(), mensagem.encode('utf-8'))
-        self.w32.WritePrinter(self.w32.GetDefaultPrinter(), b'\n\n')
+        win32print.StartDocPrinter(prt, 1, (f'{self.nome_arquivo}.png', None, None))
+        win32print.EndDocPrinter(prt)
+        win32print.ClosePrinter(prt)
         
-        # Finalização
-        with open(f'{self.nome_arquivo}.png', 'rb') as f:
-            self.w32.WritePrinter(self.w32.GetDefaultPrinter(), f.read())
-            
-        self.w32.EndPagePrinter(self.w32.GetDefaultPrinter())
-        self.w32.EndDocPrinter(self.w32.GetDefaultPrinter())
-
-        # Remover arquivos
         remove(f'{self.nome_arquivo}.png')
+        print('Imprimindo...')
+        
 App()   
