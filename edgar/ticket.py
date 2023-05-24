b899ca30ec6ce462148559353f10fe48d5378b4e
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-05-23 22:17:48 -0300

    Update ticket.py

--- edgar/ticket.py
+++ edgar/ticket.py
@@ -7,10 +7,8 @@ from sys import exit
 class App():
     def __init__(self):            
         self.nome_arquivo = 'temp_code'
-        self.w32 = win32print
         while True:
             try:
-                self.codigo = input("Bipe o código de barras do produto (ou 'sair' para encerrar): ")
                 self.gerar_cod()
                 break
             except:
@@ -18,17 +16,18 @@ class App():
                 print('Valor incorreto, Digite novamente!')   
     
     def gerar_cod(self):
+        self.codigo = input("Bipe o código de barras do produto (ou 'sair' para encerrar): ")
         self.codigo_de_barras = EAN13(str(self.codigo), writer=ImageWriter())
         self.codigo_de_barras.save(self.nome_arquivo)
         self.imprimir()
         
     def imprimir(self):
-        defp = win32print.GetDefaultPrinter()
-        prt = win32print.OpenPrinter(defp)
-        
-        win32print.StartDocPrinter(prt, 1, (f'{self.nome_arquivo}.png', None, None))
-        win32print.EndDocPrinter(prt)
-        win32print.ClosePrinter(prt)
+        impressora_definida = win32print.GetDefaultPrinter()# Definido imp padrão
+        impressora = win32print.OpenPrinter(impressora_definida)# Inicializando a impressora
+        win32print.StartDocPrinter(impressora, 1, (f'{self.nome_arquivo}.png', None, None))# Definindo o arquivo
+        # Finalizando impressora
+        win32print.EndDocPrinter(impressora)
+        win32print.ClosePrinter(impressora)
         
         print('Imprimindo...')
         remove(f'{self.nome_arquivo}.png')
