2dd27973b1acf37b00e6d2d4e668b267a6be6f9e
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-05-24 12:53:40 -0300

    .

--- sqlite/main.py
+++ sqlite/main.py
@@ -2,3 +2,13 @@ from sqlite3 import connect, Cursor
 
 conn = connect('sqlite/db/banco_dados.db')
 c = conn.cursor()
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS dados
+          (
+              id INTEGER PRIMARY KEY AUTOINCREMENT, 
+              nome TEXT,
+              cpf INTEGER,
+              dn TEXT
+          )
+          ''')
